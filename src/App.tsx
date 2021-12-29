221ce2d2caafc09b0b71f24e066fe3977e3426d3
Cristian Montes <cmontesmujica@gmail.com>
2021-12-29 00:12:35 -0700

    getting architecture build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <div className="App">
         <p>
-          Learn React
+          <Home />
         </p>
     </div>
   );
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () =>{
+    return(
+    <div>
+        <h1>Make Some Money Moves!</h1>
+        <a href="toSinginView">Make MoneyMoves</a> <br/>
+        <a href="toSingUpView"> Join MoneyMoves</a>
+    </div>
+    )
+}
+
+export default Home;
