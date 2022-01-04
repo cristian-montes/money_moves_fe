1774298425f939e97ac4dd5644508689a9abb780
Cristian Montes <cmontesmujica@gmail.com>
2022-01-04 00:04:33 -0700

    profile styling

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { transactionType, transactionsDiv, transactionContainer, section, amount, header } from "./ProfileStyles";
 import { getUserTransactionHistory } from "../../Utils/transaction-fetch-utils";
 
 // interface transactionHistory 
@@ -24,15 +25,35 @@ export default function Profile(){
 
     return(
         <div>
-            <h1>ALL OF YOUR TRANSACTIONS WILL APPER HERE</h1>
-            {transactions.map((transaction:any)=>{
-                return(
-                    <div key={`${transaction.paymentid}-`}>
-                        {Object.keys(transaction)[0] === 'senderid' ?(<p>Sent From:{transaction.name}</p> ): (<p>Received From:{transaction.name} </p>) }
-                        <p>${transaction.amount}</p>
-                    </div>
-                )
-            })}
+            <h1 style={header}>Your Transactions Activity</h1>
+            <div style={transactionContainer}>
+                {transactions.map((transaction:any)=>{
+                    return(
+                        <div key={`${transaction.paymentid}-`} style={transactionsDiv} >
+                            {Object.keys(transaction)[0] === 'senderid' 
+                                ? 
+                            (<div>
+                                <p style={transactionType}>Received From</p>
+                                <section style={section}>
+                                    <p>{transaction.name}</p>
+                                    <p>{transaction.email}</p>
+                                </section>
+                            </div>) 
+                                : 
+                            (<div>
+                                <p style={transactionType}>Payment Sent</p>
+                                <section style={section}>
+                                    <p>{transaction.name}</p>
+                                    <p>{transaction.email}</p>
+                                </section>
+                            </div>) }
+                            <div style={amount}>
+                                <p>${transaction.amount}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
 
     )
